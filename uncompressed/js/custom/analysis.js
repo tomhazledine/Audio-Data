8258e436dc1e8ffe087a0c4e71a89184f490fcd2
Tom Hazledine <tom@eatenbymonsters.com>
2016-05-02 11:01:34 +0100

    added comments to D3 functions

--- uncompressed/js/custom/analysis.js
+++ uncompressed/js/custom/analysis.js
@@ -37,46 +37,19 @@ function audioAnalysis(context,master){
         var array = new Uint8Array(volumeAnalyser.frequencyBinCount);
         // Get the audio data and store it in our array
         volumeAnalyser.getByteFrequencyData(array);
+        // If we wanted information about the waveform
+        // rather than the frequency, we would use this:
         // volumeAnalyser.getByteTimeDomainData(array);
 
+        // Calculate the mean value of the frequency frame
         var volume = getAverageVolume(array);
+
+        // Update the volume display
         redrawVolume(volume);
         redrawFrequency(array);
 
     }
 }
-
-// function audioAnalysis_frequency(context,master){
-    
-//     // Setup a frequencyAnalyser
-//     var frequencyAnalyser = context.createAnalyser();
-//     frequencyAnalyser.fftSize = 2048;
-//     frequencyAnalyser.smoothingTimeConstant = 0;
-//     var bufferSize = frequencyAnalyser.frequencyBinCount;
-//     // Connect the frequencyAnalyser to our master audio output
-//     master.connect(frequencyAnalyser);
-
-//     // Create a Node to listen to the output every
-//     // 2048 'frames' (a.k.a. 21 times a
-//     // second at sample-rate of 44.1k)
-//     listenerNode = context.createScriptProcessor(2048,1,1);
-
-//     // Connect it to our audio:
-//     listenerNode.connect(master);
-
-//     listenerNode.onaudioprocess = function(){
-
-//         // Create an array to store our data
-//         var array = new Uint8Array(frequencyAnalyser.frequencyBinCount);
-//         // Get the audio data and store it in our array
-//         frequencyAnalyser.getByteFrequencyData(array);
-//         // frequencyAnalyser.getByteTimeDomainData(array);
-
-//         var frequency = array;
-//         redrawfrequency(frequency);
-
-//     }
-// }
 
 /**
  * --------------------
@@ -115,137 +88,182 @@ function getAverageVolume(array){
 }
 
 /**
+ * ---------------------
  * DRAW VOLUME OUTPUT
+ *
+ * Use the D3.js library
+ * to create an SVG that
+ * can display volume as
+ * a simple bar chart.
+ * ---------------------
  */
 
+// Set the width and height
+// of our display.
 var vol_w = 20,
     vol_h = 80;
 
+// Creat a D3 Scale to map
+// our data to the width of
+// our display.
 var vol_x = d3.scale.linear()
     .domain([0,1])
     .range([0,vol_w]);
 
+// Creat a D3 Scale to map
+// our data to the height
+// of our display.
 var vol_y = d3.scale.pow()
     .domain([0,140])
     .rangeRound([0,vol_h]);
 
+// Append an SVG to the DOM node
+// we've created in the markup.
 var volumeOutput = d3.select('#volumeDisplay')
     .append('svg')
         .attr('class','volumeOutputChart')
         .attr('width',vol_w)
         .attr('height',vol_h);
 
+// Add a background "group" ("g") to that SVG
 var volumeOutputBackground = volumeOutput.append('g')
     .attr('class','backgroundWrapper')
     .attr('width',vol_w)
     .attr('height',vol_h)
+        // And add a rectangle within that group.
         .append('rect')
             .attr('class','background')
             .attr('width',vol_w)
             .attr('height',vol_h);
 
+// Add a "group" to contain our dynamic shapes
 var volumeOutputData = volumeOutput.append('g')
     .attr('class','dataWrapper')
     .attr('width',vol_w)
     .attr('height',vol_h);
 
+// Use placeholder data ("[0]") to generate
+// the starting-point of our bar. We do this
+// by passing the data into D3 and generating
+// rectangles for each data item (nice and
+// simple for this 1-bar volume chart).
 volumeOutputData.selectAll('rect')
     .data([0])
     .enter().append('rect')
-        .attr('x',0)// - .5)
-        .attr('y',0)//function(d){ return h - y(d) - .5; })
+        .attr('x',0)
+        .attr('y',0)
         .attr('width',vol_w)
         .attr('height',function(d){ return vol_h - vol_y(d); });
 
+// Declare a function that will redraw the rectangle
+// based on input value ("float").
+// This function gets called by our Analyser.
 function redrawVolume(float){
-
     volumeOutputData.selectAll('rect')
         .data([float])
         .transition()
             .duration(0001)
-            // .attr("y", function(d) { return h - y(d) - .5; })
+            // Update the height based on the data.
+            // We're using this rectangle as a mask
+            // over our coloured background, so we
+            // subtract the value from the total
+            // height.
             .attr('height',function(d){ return vol_h - vol_y(d); });
 }
 
+
+
+
 /**
+ * ---------------------
  * DRAW FREQUENCY OUTPUT
+ *
+ * Use the D3.js library
+ * to create an SVG that
+ * can display real-time
+ * frequency data as a
+ * a line graph.
+ * ---------------------
  */
 
+
+// Generate some placeholder data to use
+// when we initialise the graph on-load.
 var placeholderFrequencyData = [];
 for (var i = 0; i < 1024; i++) {
     placeholderFrequencyData.push(0);
 }
 
-// placeholderFrequencyData = [0, 0, 26, 11, 0, 9, 0, 0, 0, 27, 50, 49, 38, 14, 21, 68, 162, 220, 243, 234, 192, 118, 67, 123, 168, 207, 232, 237, 220, 184, 133, 75, 29, 22, 23, 22, 0, 0, 15, 14, 8, 25, 49, 52, 33, 0, 0, 0, 0, 0, 18, 83, 135, 176, 200, 204, 187, 154, 113, 64, 36, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 19, 11, 0, 7, 13, 15, 5, 0, 0, 0, 0, 0, 0, 0, 2, 42, 88, 127, 157, 177, 185, 180, 165, 141, 112, 80, 48, 13];
-
-// console.log(placeholderFrequencyData);
-
+// Set the width and height
+// of our display.
 var freq_w = 800,
     freq_h = 250;
 
+// Creat a D3 Scale to map
+// our data to the width of
+// our display.
 var freq_x = d3.scale.linear()
     .domain([0,1])
     .range([0,freq_w]);
 
+// Creat a D3 Scale to map
+// our data to the height of
+// our display.
 var freq_y = d3.scale.linear()
     .domain([0,250])
     .rangeRound([0,freq_h]);
 
+// Append an SVG to the DOM node
+// we've created in the markup.
 var frequencyOutput = d3.select('#frequencyDisplay')
     .append('svg')
         .attr('class','frequencyOutputChart')
         .attr('width',freq_w)
         .attr('height',freq_h);
 
+// Add a background "group" ("g") to that SVG
 var frequencyOutputBackground = frequencyOutput.append('g')
     .attr('class','backgroundWrapper')
     .attr('width',freq_w)
     .attr('height',freq_h)
+        // And add a rectangle within that group.
         .append('rect')
             .attr('class','background')
             .attr('width',freq_w)
             .attr('height',freq_h);
 
+// Add a "group" to contain our dynamic shapes
 var frequencyOutputData = frequencyOutput.append('g')
     .attr('class','dataWrapper')
     .attr('width',freq_w)
     .attr('height',freq_h);
 
-// frequencyOutputData.selectAll('rect')
-//     .data(placeholderFrequencyData)
-//     .enter().append('rect')
-//         .attr('x',function(d,i){ return (freq_w / placeholderFrequencyData.length) * i;})
-//         .attr('y',0)//function(d){ return freq_h - freq_y(d); })
-//         .attr('width',freq_w / placeholderFrequencyData.length)
-//         .attr('height',function(d){ return freq_y(d); });
-
+// Create a path in our SVG
 var freq_path = frequencyOutputData.append('path');
-// freq_path
-//     .data(placeholderFrequencyData)
-//     .enter()
 
-            line = d3.svg.line()
-                .x(function(d,i){ return (freq_w / 512) * i; })
-                .y(function(d){ return freq_h - freq_y(d); })
-                .interpolate('monotone');
+// Create a D3 function to set the path's
+// values based on given data.
+line = d3.svg.line()
+    .x(function(d,i){ return (freq_w / 512) * i; })
+    .y(function(d){ return freq_h - freq_y(d); })
+    .interpolate('monotone');
 
-            freq_path
-                .attr('d',line(placeholderFrequencyData))
-                .attr('fill','none')
-                .classed('y1',true)
-                .attr('stroke-width','1px');
+// Use the line() function to pass placeholder data
+// into our SVG path
+freq_path
+    .attr('d',line(placeholderFrequencyData))
+    .attr('fill','none')
+    .classed('y1',true)
+    .attr('stroke-width','1px');
 
+// Declare a function that will redraw the line
+// based on input value ("array").
+// This function gets called by our Analyser.
 function redrawFrequency(array){
 
+    // Pass new data into the line() function
+    // and use that to set the new points for
+    // the SVG path.
     freq_path
-        .attr('d',line(array))
-
-    // frequencyOutputData.selectAll('rect')
-    //     .data(array)
-    //     .transition()
-    //         .duration(0001)
-    //         .attr('x',function(d,i){ return (freq_w / array.length) * i;})
-    //         .attr('y',0)//function(d){ return freq_h - freq_y(d); })
-    //         .attr('width',freq_w / array.length)
-    //         .attr('height',function(d){ return freq_h - freq_y(d); });
+        .attr('d',line(array));
 }
