dd8a5157d8f7641d8b216e12cd86f15c7c73df45
Tom Hazledine <tom@tomhazledine.com>
2017-04-25 17:36:20 +0100

    setting up audio file test page

---
+++ three.php
@@ -0,0 +1,13 @@
+<?php include('header.php'); ?>
+
+<h1>Gendall 30 Instrument</h1>
+
+<div>
+
+    <audio src="/files/lostThatEasy.mp3" title="Lost that easy" data-artist="Cold War Kids" controls>
+        Your browser does not support the <code>audio</code> element.
+    </audio>
+
+</div>
+
+<?php include('footer.php'); ?>
