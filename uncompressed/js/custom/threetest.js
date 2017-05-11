24e80f154c9bbb65f98cba3bf249f99a08c70adb
Tom Hazledine <tom@tomhazledine.com>
2017-05-11 18:58:45 +0100

    light positions

--- uncompressed/js/custom/threetest.js
+++ uncompressed/js/custom/threetest.js
@@ -120,7 +120,7 @@ function createLights() {
     
     // A directional light shines from a specific direction. 
     // It acts like the sun, that means that all the rays produced are parallel. 
-    shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+    shadowLight = new THREE.DirectionalLight(0xffffff, .3);
 
     // Set the direction of the light  
     shadowLight.position.set(150, 350, 350);
@@ -141,9 +141,17 @@ function createLights() {
     shadowLight.shadow.mapSize.width = 2048;
     shadowLight.shadow.mapSize.height = 2048;
 
-    pointLight = new THREE.DirectionalLight(0xffffff, .9);
-    pointLight.position.set(-150, 150, 150);
+    pointLight = new THREE.DirectionalLight(0xffffff, .3);
+    pointLight.position.set(150, 200, 0);
     pointLight.castShadow = true;
+    pointLight.shadow.camera.left = -400;
+    pointLight.shadow.camera.right = 400;
+    pointLight.shadow.camera.top = 400;
+    pointLight.shadow.camera.bottom = -400;
+    pointLight.shadow.camera.near = 1;
+    pointLight.shadow.camera.far = 1000;
+    pointLight.shadow.mapSize.width = 2048;
+    pointLight.shadow.mapSize.height = 2048;
 
     
     // to activate the lights, just add them to the scene
